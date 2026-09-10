ab72a50115885729596797f5898bdc565e4942b9
CUB3D <callumthom11@gmail.com>
2026-09-10 08:06:22 +0100

    Figured out the DB upx encryption

--- genericfs/src/upx.rs
+++ genericfs/src/upx.rs
@@ -2,13 +2,21 @@
 //! https://github.com/stricq/UPKManager/blob/master/UPK_Format.pdf
 
 use crate::file_ref::FileRef;
+use crate::gen_item::{BufGenItm, GenItem};
+use crate::generic_fs::GenFS;
 use crate::generic_fs_props::GenFSProps;
-use crate::{gen_item::GenItem, generic_fs::GenFS};
 use memmap2::Mmap;
-use parse::{le_i32, le_u32, take, take_arr, take_vec};
-use rust_lzo::LZOContext;
+use parse::{le_i32, le_u32, le_u64, take, take_arr, take_vec};
+use rust_lzo::{LZOContext, LZOError};
 
-pub struct UpxF;
+use aes::cipher::{BlockDecrypt, KeyInit};
+use aes::Aes256;
+use aes::cipher::generic_array::GenericArray;
+
+pub struct UpxF {
+    upx: UpxFile,
+    idx: usize,
+}
 
 impl GenFSProps for UpxF {
     const FORMAT_NAME: &'static str = "upx";
@@ -18,7 +26,12 @@ fn fstring(i: &[u8]) -> anyhow::Result<(&[u8], String)> {
     let (i, len) = le_i32(i)?;
     assert!(len > 0); // ascii, null term
     let (i, s) = take(i, len as usize)?;
-    Ok((i, String::from_utf8_lossy(s).to_string()))
+    Ok((
+        i,
+        String::from_utf8_lossy(s)
+            .trim_end_matches('\0')
+            .to_string(),
+    ))
 }
 
 #[derive(Debug)]
@@ -27,22 +40,6 @@ pub struct FCompChunk {
     pub uncom_sz: u32,
     pub comp_sz: u32,
     pub comp_off: u32,
-}
-
-fn fcompchunk(i: &[u8]) -> anyhow::Result<(&[u8], FCompChunk)> {
-    let (i, uncom_of) = le_u32(i)?;
-    let (i, uncom_sz) = le_u32(i)?;
-    let (i, comp_off) = le_u32(i)?;
-    let (i, comp_sz) = le_u32(i)?;
-    Ok((
-        i,
-        FCompChunk {
-            uncom_of,
-            uncom_sz,
-            comp_sz,
-            comp_off,
-        },
-    ))
 }
 
 #[derive(Debug)]
@@ -89,27 +86,15 @@ pub struct UpxFile {
     pub cookver: u32,
     pub compflag: u32,
     pub chunks: Vec<FCompChunk>,
+    pub content: Vec<u8>,
 }
 
 impl GenFS for UpxF {
-    fn try_open_internal(_f: &FileRef) -> anyhow::Result<Self>
+    fn try_open_internal(f: &FileRef) -> anyhow::Result<Self>
     where
         Self: Sized,
     {
-        Ok(Self {})
-    }
-
-    fn sniff(f: &Mmap) -> anyhow::Result<bool>
-    where
-        Self: Sized,
-    {
-        let i = &f[..];
-
-        let (i, sig) = le_u32(i)?;
-        if sig != 0x9e2a83c1 {
-            return Ok(false);
-        }
-
+        let (i, _sig) = le_u32(&f.mmap[..])?;
         let (i, ver) = le_u32(i)?;
         let (i, hdrsz) = le_u32(i)?;
 
@@ -135,14 +120,31 @@ impl GenFS for UpxF {
         let (i, enginever) = le_u32(i)?;
         let (i, cookver) = le_u32(i)?;
         let (i, compflag) = le_u32(i)?;
-        assert_eq!(compflag, 0x202); // Dirty Bomb, LZO1X
+        if compflag & 0xf != 2 {
+            anyhow::bail!("compressionFlags must be LZO {compflag:#x}");
+        }
 
         let (i, num_chunks) = le_u32(i)?;
-        let (_i, chunks) = take_vec(i, num_chunks as _, fcompchunk)?;
-        println!("{:?}", chunks);
-        assert_eq!(chunks.len(), 1);
 
-        let str = UpxFile {
+        fn fcompchunk(i: &[u8]) -> anyhow::Result<(&[u8], FCompChunk)> {
+            let (i, uncom_of) = le_u32(i)?;
+            let (i, uncom_sz) = le_u32(i)?;
+            let (i, comp_off) = le_u32(i)?;
+            let (i, comp_sz) = le_u32(i)?;
+            Ok((
+                i,
+                FCompChunk {
+                    uncom_of,
+                    uncom_sz,
+                    comp_sz,
+                    comp_off,
+                },
+            ))
+        }
+
+        let (_i, chunks) = take_vec(i, num_chunks as _, fcompchunk)?;
+
+        let mut upx = UpxFile {
             ver,
             hdrsz,
             pkg,
@@ -164,56 +166,96 @@ impl GenFS for UpxF {
             cookver,
             compflag,
             chunks,
+            content: Vec::new(),
         };
 
-        // for c in chunks {
-        //     let mut out = vec![0u8; c.uncom_sz as _];
-        //     let inp = &f[c.comp_off as usize-1..][..c.comp_sz as usize];
-        //
-        //     let mut tmp = vec![0u8; 0x20000];
-        //     tmp[..inp.len()].copy_from_slice(inp);
-        //
-        //     let dec = lzo1x::decompress(&tmp, &mut out);
-        //     println!("dec res - {:?}", dec);
+        println!("{:x} {} {} {} {}", nameoff, expoff, impoff, depoff, seroff);
 
-        let _fchunk = str.chunks.first().unwrap();
-
-        let mut out = vec![0u8; 226078];
-        for off in 0..f.len() {
-            let mut v = vec![0u8; 226078];
-            out.fill(0);
-
-            let inp = &f[off..];
-            let inp = if inp.len() > v.len() {
-                &inp[..v.len()]
-            } else {
-                inp
-            };
-            v[..inp.len()].copy_from_slice(inp);
-
-            // let x = lzo1x::decompress(&v, &mut out);
-            let (dec, err) = LZOContext::decompress_to_slice(&v, &mut out);
-
-            if off < 500 {
-                std::fs::write("./test.bin", &dec).unwrap();
+        for c in &upx.chunks {
+            let base = c.comp_off as usize;
+            if base + c.comp_sz as usize > f.mmap.len() {
+                panic!("chunk too big")
             }
 
-            let err = err as i32;
-            if err == 0 || err == -8 {
-                println!("{:?}", off);
+            let (i, tag) = le_u32(&f.mmap[base..])?;
+            let (i, blk_sz) = le_u32(i)?;
+            let (i, _total_comp) = le_u32(i)?;
+            let (i, uncomp_sz) = le_u32(i)?;
+
+            assert_eq!(tag, 0x9e2a83c1);
+            assert_eq!(uncomp_sz, c.uncom_sz);
+
+            let blk_cnt = (uncomp_sz + blk_sz - 1) / blk_sz;
+            let end = c.uncom_of as usize + c.uncom_sz as usize;
+            if upx.content.len() < end {
+                upx.content.resize(end, 0);
+            }
+
+            let mut doff = base+16 + blk_cnt as usize * 8;
+            let mut cur = c.uncom_of as usize;
+            let mut i = i;
+            for _bid in 0..blk_cnt {
+                let (j, comp_sz) = le_u32(i)?;
+                let (j, uncomp_sz) = le_u32(j)?;
+                i = j;
+
+                let inp = &f.mmap[doff..][..comp_sz as usize];
+                let (dec, err) =
+                    LZOContext::decompress_to_slice(inp, &mut upx.content[cur..][..uncomp_sz as usize]);
+                if err != LZOError::OK {
+                    panic!("failed to decompress upx");
+                }
+                assert_eq!(dec.len(), uncomp_sz as usize);
+
+                doff += comp_sz as usize;
+                cur += uncomp_sz as usize;
+            }
+
+            if upx.compflag & 0x200 != 0 {
+                let start = c.uncom_of as usize;
+                let enc_len = c.uncom_sz as usize & !0xf;
+
+                let aes = Aes256::new_from_slice(b"sdjJKLJsklaJSLKJDWLZMXNsldjKjalk")?;
+                for c in &mut upx.content[start..][..enc_len].chunks_exact_mut(16) {
+                    aes.decrypt_block(GenericArray::from_mut_slice(c));
+                }
             }
         }
-        // }
 
-        //TODO: we can read the hdr fine
-        // we can't do the decomp, some libs are too strict and this one doesn't produce enough output
-        // It might be worth hooking the lzo func with frida and dumping what the actual args are
+        let foo = upx.chunks.iter().map(|c| c.uncom_of).min().unwrap() as usize;
+        let mut full = f.mmap[..foo].to_vec();
+        full.extend_from_slice(&upx.content[foo..]);
 
-        Ok(true)
+        let i = &upx.content[upx.expoff as usize..];
+        let (i, ty) = le_u32(i)?;
+        let (i, parent) = le_u32(i)?;
+        let (i, owner) = le_u32(i)?;
+        let (i, name) = le_u64(i)?;
+        let (i, archtype) = le_u32(i)?;
+        let (i, objflag1) = le_u32(i)?;
+        let (i, objflag2) = le_u32(i)?;
+        let (i, sz) = le_u32(i)?;
+        let (_i, _off) = le_u32(i)?;
+
+        println!("{ty} {parent} {owner} {name} {archtype} {objflag1} {objflag2} {sz}");
+
+
+        Ok(Self { upx, idx: 0 })
+    }
+
+    fn sniff(f: &Mmap) -> anyhow::Result<bool>
+    where
+        Self: Sized,
+    {
+        Ok(f.get(0..4) == Some(&[0xc1, 0x83, 0x2a, 0x9e]))
     }
 
     fn next_itm(&mut self) -> anyhow::Result<Option<Box<dyn GenItem>>> {
-        Ok(None)
+        if self.idx == 1 {
+            return Ok(None);
+        }
+        self.idx+=1;
+        Ok(Some(Box::new(BufGenItm::new(format!("{}.u.dec", self.upx.pkg), self.upx.content.clone()))))
     }
 
     fn name(&self) -> &str {
